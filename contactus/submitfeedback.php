90b8b98def4b5ba44b2cbcc00c800fdda6351e7d
ujjawal23 <78785865+ujjawal23@users.noreply.github.com>
2023-05-31 23:33:59 +0530

    Update submitfeedback.php

--- contactus/submitfeedback.php
+++ contactus/submitfeedback.php
@@ -1,14 +1,13 @@
 <?php 
 
 session_start(); 
-
+$uid=$_SESSION['uid'];
 
 include "../includes/db.php";
 $mobile=$_POST['phone'];
 $email=$_POST['email'];
 $uname=$_POST['name'];
 $msg=$_POST['msg'];
-
 
 
 if(isset($_POST['submit'])){
@@ -21,7 +20,7 @@ if (empty($mobile ||$email || $uname )) {
 }else{
    
          // Insert image file name into database
-         $insert = $conn->query("INSERT into feedback (`name`, `phone` ,`email`,`msg`) VALUES ('$uname',' $mobile','$email','$msg')");
+         $insert = $conn->query("INSERT into feedback (`name`, `phone` ,`email`,`msg`,`uid`) VALUES ('$uname',' $mobile','$email','$msg','$uid')");
          echo $insert;
         
          if($insert){
