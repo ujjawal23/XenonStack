197f9463143285de41728c549e0e6949e975781c
ujjawal23 <78785865+ujjawal23@users.noreply.github.com>
2023-06-01 10:49:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
+<style>.signup{
+  color:black;
+
+}
+  </style>
+  <!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+<meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <html lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    </head>
+    <body></body>
+
 <div class="parent clearfix">
     <div class="bg-illustration">
-      <img src="https://i.ibb.co/Pcg0Pk1/logo.png" alt="logo">
+      <img src="https://logomaker.designfreelogoonline.com/media/productdesigner/logo/resized/000749_online_store_logos_design_free_online_E-commerce_cart_logo_maker-02.png" alt="logo">
       <link rel="stylesheet" href="style.css">
   
       <div class="burger-btn">
@@ -19,11 +38,15 @@
           <form action="login.php" method="post">
             <input type="email " name="email" placeholder="E-mail Address">
             <input type="password" name="password" placeholder="Password">
-            <button type="submit" name="submit">LOG-IN</button>
+            <button style="background-color:#6362ad;"type="submit" name="submit">LOG-IN</button>
   
           </form>
+          <a href="signup" class="signup"type="submit" name="sign-up"><u>Sign Up</u></a>
+
         </div>
   
       </div>
     </div>
   </div>
+  </body>
+  </html>
