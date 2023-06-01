783236f89710a7fac0c0632eb39ea7adc92b6487
ujjawal23 <78785865+ujjawal23@users.noreply.github.com>
2023-06-01 10:50:29 +0530

    Add files via upload

---
+++ signup/index.php
@@ -0,0 +1,40 @@
+<style>.Login{
+  color:black;
+
+}
+  </style>
+
+<div class="parent clearfix">
+    <div class="bg-illustration">
+      <img src="https://i.ibb.co/Pcg0Pk1/logo.png" alt="logo">
+      <link rel="stylesheet" href="../style.css">
+  
+      <div class="burger-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+  
+    </div>
+  
+    <div class="login">
+      <div class="container">
+        <h1>Sign Up to access</h1>
+  
+        <div class="login-form">
+          <form action="signup.php" method="post">
+          <input type="name " name="name" placeholder="Name">
+          <input type="phone " name="phone" placeholder="phone">
+
+            <input type="email " name="email" placeholder="E-mail Address">
+            <input type="password" name="password" placeholder="Password">
+            <button style="background-color:#6362ad;"type="submit" name="submit">Sign Up</button>
+  
+          </form>
+          <a href="../" class="Login" name="Log-In"><u>Log In</u></a>
+
+        </div>
+  
+      </div>
+    </div>
+  </div>
---
+++ signup/signup.php
@@ -0,0 +1,70 @@
+<?php 
+
+session_start(); 
+
+
+include "..\includes/db.php";
+$mobile=$_POST['phone'];
+$email=$_POST['email'];
+$uname=$_POST['name'];
+$pass=$_POST['password'];
+
+
+if (empty($_POST['name'] || $_POST['email'] || $_POST['phone'] || $_POST['password'])) {
+    // One or more values are empty.
+    echo' <script>alert("Please complete the registration form!")
+    history.back()
+    </script>';
+}else{
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo' <script>alert("Email is not valid!")
+        history.back()
+        </script>';
+        exit();
+    }elseif (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+        echo' <script>alert("Password must be between 5 and 20 characters long!")
+        history.back()
+        </script>';
+        exit();
+    }elseif (preg_match('/^[a-zA-Z0-9]+$/', $_POST['password']) == 0) {
+        echo' <script>alert("password should not contain /^[a-zA-Z0-9]+$/!")
+        history.back()
+        </script>';
+        exit();
+    }elseif (strlen($_POST['phone']) < 10 || strlen($_POST['phone']) <10) {
+        echo' <script>alert("enter valid number without country code")
+        history.back()
+        </script>';
+        exit();
+    }else{
+    $user = "SELECT * FROM `user` WHERE `phone`='$mobile' ";
+    $user_run=mysqli_query($conn,$user);
+    $usere = "SELECT * FROM `user` WHERE `email`='$email' ";
+    $usere_run=mysqli_query($conn,$usere);
+    if(mysqli_num_rows($user_run)>0){
+        echo' <script>alert("mobile alredy exists")
+        history.back()
+                </script>';
+                exit();
+
+    }elseif(mysqli_num_rows($usere_run)>0){
+        echo' <script>alert("email alredy exists")
+        history.back()
+                </script>';
+                exit();
+    }else{
+         // Insert image file name into database
+         $insert = $conn->query("INSERT into user (name, phone ,email,pass) VALUES ('$uname',' $mobile','$email','$pass')");
+        
+         if($insert){
+            echo' <script>alert("User registered successfully")
+                location.href = "../"
+                </script>';
+                exit();
+
+         }
+    }
+
+    }
+}
+?>
